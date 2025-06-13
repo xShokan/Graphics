6a2f9173d2c4b2dc41bf3460018fed8f4bc9f776
shokanxu <shokanxu@tencent.com>
2025-06-13 23:13:12 +0800

    Init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+// #include <glad/glad.h>
+#include <GL/gl.h>
+
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    return 0;
+}
